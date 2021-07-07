b178a891a4d028359e38baf1f081056af151558e
afmika <afmichael73@gmail.com>
2021-07-07 10:14:06 +0300

    [ADD] afquery -- hib+afquery mapper

--- afsql-cli.js
+++ afsql-cli.js
@@ -3,6 +3,8 @@
 const SQLParser = require(__dirname.replace(/\\/gi, '/') + '/scripts/SQLParser');
 const mapJavaFrom = require(__dirname.replace(/\\/gi, '/') + '/JavaMapper');
 const mapJavaHibernateFrom = require(__dirname.replace(/\\/gi, '/') + '/JavaHibernateMapper');
+const mapJavaAfQueryFrom = require(__dirname.replace(/\\/gi, '/') + '/JavaAfQueryMapper');
+const JavaHibernateAndAfQueryMapper = require(__dirname.replace(/\\/gi, '/') + '/JavaHibernateAndAfQueryMapper');
 const mapJSFrom = require(__dirname.replace(/\\/gi, '/') + '/JSMapper');
 
 const [ , , ...args] = process.argv;
@@ -14,9 +16,14 @@ function sqlFromFile (sqlfile, op, use_spaces = false) {
 		const temp = SQLParser.parse(query);
 		let filename = temp.table_name;
 		let output = null;
-		if (['java', 'hibernate'].includes(op)) {
+		if (['java', 'hibernate', 'afquery', 'hibernate-afquery'].includes(op)) {
 			filename += '.java';
-			let fun_map = {'java' : mapJavaFrom, 'hibernate' : mapJavaHibernateFrom};
+			let fun_map = {
+				'java' : mapJavaFrom, 
+				'hibernate' : mapJavaHibernateFrom,
+				'hibernate-afquery' : JavaHibernateAndAfQueryMapper, 
+				'afquery' : mapJavaAfQueryFrom
+			};
 			output = fun_map[op] (query, non_public);			
 		} else {
 			filename += '.js';
@@ -56,7 +63,7 @@ function badOption () {
 // afsql sql file.sql
 const [op, file, use_spaces] = args;
 if (args.length == 2 || args.length == 3) {
-	if (['java', 'js', 'hibernate'].includes(op)) {
+	if (['java', 'js', 'hibernate', 'afquery', 'hibernate-afquery'].includes(op)) {
 		console.log(use_spaces == '--spaces' ? 'Mode : Spaces' : 'Mode : Tabs');
 		sqlFromFile(file, op, use_spaces == '--spaces');
 	} else {
